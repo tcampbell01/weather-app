b54218d8140244b5e367523f119f3e4dfa088424
TC <teresa.j.campbell@gmail.com>
2021-12-24 15:31:42 -0600

    added weather API

--- css/js/index.js
+++ css/js/index.js
@@ -1,17 +1,18 @@
+var weatherCity=document.querySelector(".city-input");
 
-function weather( city ) {
+function weather( weatherCity ) {
     var key = '{cd896d7cd9e9b2967e53ada8c52918e6}';
-    fetch('https://api.openweathermap.org/data/2.5/weather?id=' + city+ '&appid=' + key)  
+    fetch(api.openweathermap.org/data/2.5/forecast?q='weatherCity' + 'key'
     .then(function(resp) { return resp.json() }) // Convert data to json
     .then(function(data) {
       console.log(data);
     })
     .catch(function() {
       // catch any errors
-    });
-  }
+    })
   
-  window.onload = function() {
-      weather(Minneapolis);
-  }
+  
+ // window.onload = function() {
+      //weather(weatherCity)
+ // 
     
