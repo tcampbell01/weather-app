d4086770d73fdcf15bb22ac53891206cc2f09879
TC <teresa.j.campbell@gmail.com>
2021-12-26 10:53:36 -0600

    city input value

--- css/js/index.js
+++ css/js/index.js
@@ -1,18 +1,35 @@
-var weatherCity=document.querySelector(".city-input");
 
-function weather( weatherCity ) {
-    var key = '{cd896d7cd9e9b2967e53ada8c52918e6}';
-    fetch(api.openweathermap.org/data/2.5/forecast?q='weatherCity' + 'key'
-    .then(function(resp) { return resp.json() }) // Convert data to json
-    .then(function(data) {
-      console.log(data);
-    })
-    .catch(function() {
-      // catch any errors
-    })
-  
-  
- // window.onload = function() {
-      //weather(weatherCity)
- // 
+
+
+
+
+function getCity() {
+    var cityInput = document.getElementById("city-input").value;
     
+
+}
+
+getCity();
+console.log(cityInput);
+
+//var key = '{cd896d7cd9e9b2967e53ada8c52918e6}';
+
+//document.getElementById("input").onclick = function () {
+
+    //function weather() {
+       
+        //fetch('api.openweathermap.org/data/2.5/forecast?q=(weatherCity)&appid=(key)')
+
+            //.then(response=>response.json())
+            //.then(data=>console.log(data))
+    //}
+
+
+
+            
+        
+
+
+    
+
+    //weather();
