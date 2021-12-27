51d7aec3fb644bd85eb467e1f3ddb591e95d2ffb
TC <teresa.j.campbell@gmail.com>
2021-12-27 12:09:55 -0600

    api working

--- css/js/index.js
+++ css/js/index.js
@@ -1,4 +1,4 @@
-
+var key = 'cd896d7cd9e9b2967e53ada8c52918e6';
 
 
 document.getElementById("input").onclick = function () {
@@ -6,25 +6,26 @@ document.getElementById("input").onclick = function () {
 function getCity() {
     var cityInput = document.getElementById("city-input").value;
     console.log(cityInput);
+   
+    fetch ('https://api.openweathermap.org/data/2.5/forecast?q=' +cityInput+ '&appid=' + key)
 
+        .then(response=>response.json())
+        .then(data=>console.log(data))
 }
 
 getCity();
+
+
+
 }
 
 
-//var key = '{cd896d7cd9e9b2967e53ada8c52918e6}';
 
-//document.getElementById("input").onclick = function () {
 
-    //function weather() {
-       
-        //fetch('api.openweathermap.org/data/2.5/forecast?q=(weatherCity)&appid=(key)')
 
-            //.then(response=>response.json())
-            //.then(data=>console.log(data))
-    //}
 
+
+    
 
 
             
