3049d30236c7710a3b4ffb1f1fffacac17c20f8a
TC <teresa.j.campbell@gmail.com>
2021-12-28 15:03:36 -0600

    correct array numbers

--- css/js/index.js
+++ css/js/index.js
@@ -1,6 +1,8 @@
 var key = 'cd896d7cd9e9b2967e53ada8c52918e6';
 var temp = document.getElementById("temp");
 
+//var tempIcon=
+
 var selectedCities = [];
 
 
@@ -58,35 +60,47 @@ function getCity(){
         //const wind = data.wind.speed;
         //const humidity = data.humidity;
         document.getElementById('temp1').innerHTML="Temp:" + info.list[1].main.temp + '&deg' + 'F';
-        document.getElementById('temp2').innerHTML="Temp:" + info.list[2].main.temp + '&deg' + 'F';
-        document.getElementById('temp3').innerHTML="Temp:" + info.list[3].main.temp + '&deg' + 'F';
-        document.getElementById('temp4').innerHTML="Temp:" + info.list[4].main.temp + '&deg' + 'F'; 
-        document.getElementById('temp5').innerHTML="Temp:" + info.list[5].main.temp + '&deg' + 'F';
+        document.getElementById('temp2').innerHTML="Temp:" + info.list[9].main.temp + '&deg' + 'F';
+        document.getElementById('temp3').innerHTML="Temp:" + info.list[17].main.temp + '&deg' + 'F';
+        document.getElementById('temp4').innerHTML="Temp:" + info.list[25].main.temp + '&deg' + 'F'; 
+        document.getElementById('temp5').innerHTML="Temp:" + info.list[33].main.temp + '&deg' + 'F';
 
         document.getElementById('wind1').innerHTML="Wind:" + info.list[1].wind.speed +' MPH';
-        document.getElementById('wind2').innerHTML="Wind:" + info.list[2].wind.speed +' MPH';
-        document.getElementById('wind3').innerHTML="Wind:" + info.list[3].wind.speed +' MPH';
-        document.getElementById('wind4').innerHTML="Wind:" + info.list[4].wind.speed +' MPH';
-        document.getElementById('wind5').innerHTML="Wind:" + info.list[5].wind.speed +' MPH';
+        document.getElementById('wind2').innerHTML="Wind:" + info.list[9].wind.speed +' MPH';
+        document.getElementById('wind3').innerHTML="Wind:" + info.list[17].wind.speed +' MPH';
+        document.getElementById('wind4').innerHTML="Wind:" + info.list[25].wind.speed +' MPH';
+        document.getElementById('wind5').innerHTML="Wind:" + info.list[33].wind.speed +' MPH';
         document.getElementById('humidity1').innerHTML="Humidity:" + info.list[1].main.humidity + '%';
-        document.getElementById('humidity2').innerHTML="Humidity:" + info.list[2].main.humidity + '%';
-        document.getElementById('humidity3').innerHTML="Humidity:" + info.list[3].main.humidity + '%';
-        document.getElementById('humidity4').innerHTML="Humidity:" + info.list[4].main.humidity + '%';
-        document.getElementById('humidity5').innerHTML="Humidity:" + info.list[5].main.humidity + '%';
+        document.getElementById('humidity2').innerHTML="Humidity:" + info.list[9].main.humidity + '%';
+        document.getElementById('humidity3').innerHTML="Humidity:" + info.list[17].main.humidity + '%';
+        document.getElementById('humidity4').innerHTML="Humidity:" + info.list[25].main.humidity + '%';
+        document.getElementById('humidity5').innerHTML="Humidity:" + info.list[33].main.humidity + '%';
 
         document.getElementById('date1').innerHTML=info.list[1].dt_txt;
-        document.getElementById('date2').innerHTML=info.list[2].dt_txt;
-        document.getElementById('date3').innerHTML=info.list[3].dt_txt;
-        document.getElementById('date4').innerHTML=info.list[4].dt_txt;
-        document.getElementById('date5').innerHTML=info.list[5].dt_txt;
+        document.getElementById('date2').innerHTML=info.list[9].dt_txt;
+        document.getElementById('date3').innerHTML=info.list[17].dt_txt;
+        document.getElementById('date4').innerHTML=info.list[25].dt_txt;
+        document.getElementById('date5').innerHTML=info.list[33].dt_txt;
+
+        
+
+        
+
+
+
+        
+        
       }
-      
       )
+      
+    }
 
   
     
-    }
-    weatherForecast();
+    
+    
+
+weatherForecast();
 }
 
 getCity();
@@ -112,6 +126,8 @@ getCity();
 
             
         
+
+
 
 
 }
