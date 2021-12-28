4ab05ef3e8821140dbf29c8ca69fb3126f716f25
TC <teresa.j.campbell@gmail.com>
2021-12-28 14:14:01 -0600

    forecast card info working

--- css/js/index.js
+++ css/js/index.js
@@ -63,8 +63,22 @@ function getCity(){
         document.getElementById('temp4').innerHTML="Temp:" + info.list[4].main.temp + '&deg' + 'F'; 
         document.getElementById('temp5').innerHTML="Temp:" + info.list[5].main.temp + '&deg' + 'F';
 
-        //document.getElementById('wind1').innerHTML="Wind:" + data.wind.speed +' MPH';
-        //document.getElementById('humidity1').innerHTML="Humidity:" + data.main.humidity + '%';
+        document.getElementById('wind1').innerHTML="Wind:" + info.list[1].wind.speed +' MPH';
+        document.getElementById('wind2').innerHTML="Wind:" + info.list[2].wind.speed +' MPH';
+        document.getElementById('wind3').innerHTML="Wind:" + info.list[3].wind.speed +' MPH';
+        document.getElementById('wind4').innerHTML="Wind:" + info.list[4].wind.speed +' MPH';
+        document.getElementById('wind5').innerHTML="Wind:" + info.list[5].wind.speed +' MPH';
+        document.getElementById('humidity1').innerHTML="Humidity:" + info.list[1].main.humidity + '%';
+        document.getElementById('humidity2').innerHTML="Humidity:" + info.list[2].main.humidity + '%';
+        document.getElementById('humidity3').innerHTML="Humidity:" + info.list[3].main.humidity + '%';
+        document.getElementById('humidity4').innerHTML="Humidity:" + info.list[4].main.humidity + '%';
+        document.getElementById('humidity5').innerHTML="Humidity:" + info.list[5].main.humidity + '%';
+
+        document.getElementById('date1').innerHTML=info.list[1].dt_txt;
+        document.getElementById('date2').innerHTML=info.list[2].dt_txt;
+        document.getElementById('date3').innerHTML=info.list[3].dt_txt;
+        document.getElementById('date4').innerHTML=info.list[4].dt_txt;
+        document.getElementById('date5').innerHTML=info.list[5].dt_txt;
       }
       
       )
